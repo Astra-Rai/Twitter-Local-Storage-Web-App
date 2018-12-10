3100215acf259d7931974e846f88e9f037f716ef
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-12-09 22:26:42 -0600

    Local-Storage-App-Part-1-With Comments

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-
-
-  
 //Event Listners
 
+//call the event listner function
 eventListeners();
 
 
 function eventListeners() {
-
+  
+//form has id = "form", event listen
+//first event listener we need is for when the form is submitted
+//when the submit button is clicked, the funciton newTweet will run
+//there's for the input element, the type is "submit"  
 document.querySelector('#form').addEventListener('submit', newTweet); 
 
 }
 
 //Functions
 
+//function new tweet, we want to prevent the defulat 
 function newTweet(e){
 	e.preventDefault();
-  console.log('Form Submitted')
+//the defalut behavior is prevented
+//type text in text area field, click on submit button
+//in the console you should see "Form Submitted"  
+  console.log('Form Submitted');
 }
